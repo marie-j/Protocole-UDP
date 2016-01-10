35cbbdd6b44403347f05458edd171c2926a1848e
marie <marie.jones@etudiant.univ-lille1.fr>
2016-01-10 12:05:01 +0100

    UDP, multicast non-fonctional

---
+++ src/Multicast/SendMessages.java
@@ -0,0 +1,23 @@
+package Multicast;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class SendMessages {
+	
+	public static void main(String[] args) throws IOException {
+		
+		InetAddress destination = InetAddress.getByName("244.0.0.1");
+		int port = 7654;
+		byte[] msg = args[0].getBytes();
+		
+		DatagramPacket packet = new DatagramPacket(msg,msg.length,destination,port);
+		MulticastSocket socket = new MulticastSocket();
+		
+		socket.send(packet);
+		socket.close();
+	}
+
+}
---
+++ src/UDP/SendUDP.java
@@ -0,0 +1,21 @@
+package UDP;
+
+import java.io.IOException;
+import java.net.*;
+
+public class SendUDP {
+	
+	public static void main(String[] args) throws IOException {
+		
+		InetAddress destination = InetAddress.getByName(args[0]);
+		
+		int port = Integer.parseInt(args[1]);
+		byte[] msg = args[2].getBytes();
+		
+		DatagramPacket packet = new DatagramPacket(msg,msg.length,destination,port);
+		DatagramSocket socket = new DatagramSocket();
+		
+		socket.send(packet);
+		socket.close();
+	}
+}
