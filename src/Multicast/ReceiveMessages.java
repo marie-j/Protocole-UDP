ed452674fd7ae927d6b1e28d09e76948c7688a31
marie-j <marie.jones@etudiant.univ-lille1.fr>
2016-01-11 14:01:23 +0100

    Solve multicast problem (wrong address)

--- src/Multicast/ReceiveMessages.java
+++ src/Multicast/ReceiveMessages.java
@@ -10,11 +10,12 @@ public class ReceiveMessages {
 	public static void main(String[] arg) throws IOException {
 		
 		int port = 7654;
-		InetAddress address = InetAddress.getByName("244.0.0.1");
+		InetAddress address = InetAddress.getByName("224.0.0.1");
 		MulticastSocket socket = new MulticastSocket(port);
+		DatagramPacket packet = new DatagramPacket(new byte[1024],1024);
+		
 		socket.joinGroup(address);
 		
-		DatagramPacket packet = new DatagramPacket(new byte[1024],1024);
 		socket.receive(packet);
 			
 		String tmp = new String(packet.getData());
